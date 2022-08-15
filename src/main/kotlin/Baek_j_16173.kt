a1ae9577d1db1cf4c2a6d667a2c5d848850adb1e
박재홍 <prk0487@gmail.com>
2022-08-15 19:43:32 +0900

    20220815

--- src/main/kotlin/Baek_j_16173.kt
+++ src/main/kotlin/Baek_j_16173.kt
@@ -1,9 +1,31 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.*
+
+data class Check(var num : Int, var check: Boolean)
 
 fun main() {
 
     val br = BufferedReader(InputStreamReader(System.`in`))
 
-    val N = br.readLine()
+    val N = br.readLine().toInt()
+    var firstCheck = Check(0,false)
+    var secondCheck = firstCheck
+
+    for(i in 0 until N){
+        val temp = br.readLine()
+        val st = StringTokenizer(temp)
+
+        while(st.hasMoreTokens()){
+            val input = st.nextToken().toInt()
+            if(st.countTokens() == 0 || i == N-1){
+
+                if(input == st.countTokens()+(N-i-1))
+                else {
+
+                }
+            }
+        }
+    }
+    print("Hing")
 }

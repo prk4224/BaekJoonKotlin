74b46c07a6d541f9436f6c3eb227eec151f40b29
박재홍 <prk0487@gmail.com>
2022-08-16 18:26:59 +0900

    20220816

--- src/main/kotlin/Baek_j_16173.kt
+++ src/main/kotlin/Baek_j_16173.kt
@@ -1,30 +1,53 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
+import kotlin.collections.ArrayList
 
-data class Check(var num : Int, var check: Boolean)
+data class Map(var col : Int, var row : Int)
 
 fun main() {
 
+    val dx = arrayOf(1,0)
+    val dy = arrayOf(0,1)
+
     val br = BufferedReader(InputStreamReader(System.`in`))
 
-    val N = br.readLine().toInt()
-    var firstCheck = Check(0,false)
-    var secondCheck = firstCheck
+    val num = br.readLine().toInt()
+    val inputMap = ArrayList<ArrayList<Int>>()
 
-    for(i in 0 until N){
-        val temp = br.readLine()
-        val st = StringTokenizer(temp)
+
+    for(i in 0 until num){
+        val str = br.readLine()
+        val st = StringTokenizer(str)
+        var temp = ArrayList<Int>()
 
         while(st.hasMoreTokens()){
             val input = st.nextToken().toInt()
-            if(st.countTokens() == 0 || i == N-1){
+            println(input)
+            temp.add(input)
+        }
+        inputMap.add(temp)
+    }
 
-                if(input == st.countTokens()+(N-i-1))
-                else {
+    // BFS
+    val que : Queue<Map> = LinkedList()
+    que.offer(Map(0,0))
 
-                }
-            }
+    while(!que.isEmpty()){
+        val curr = que.poll()
+
+        if(inputMap[curr.col][curr.row] == -1) {
+            println("HaruHaru")
+            return
+        }
+
+        if(inputMap[curr.col][curr.row] == 0) continue
+
+        for(i in 0 .. 1){
+            var nx = curr.col + (dx[i]*inputMap[curr.col][curr.row])
+            var ny = curr.row + (dy[i]*inputMap[curr.col][curr.row])
+
+            if(nx >= 0 && ny >= 0 && nx < num && ny < num) que.offer(Map(nx,ny))
         }
     }
     print("Hing")

ab80db31e7a1fbc286d7b1fce1ea1ab2164748ec
박재홍 <prk0487@gmail.com>
2022-08-25 20:43:03 +0900

    20220825

---
+++ src/main/kotlin/ProgrammersKakaoReportresult.kt
@@ -0,0 +1,17 @@
+import java.util.*
+
+fun main(){
+
+}
+
+fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
+    var answer: IntArray = intArrayOf()
+
+    for(i in report.indices){
+        val st = StringTokenizer(report[i])
+
+
+    }
+
+    return answer
+}

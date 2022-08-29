1a59c56c1d8fde10b5b9dfd38f33f0566ad0f1e2
박재홍 <prk0487@gmail.com>
2022-08-30 00:05:17 +0900

    20220830

--- src/main/kotlin/ProgrammersKaKaoNewId.kt
+++ src/main/kotlin/ProgrammersKaKaoNewId.kt
@@ -48,8 +48,8 @@ fun solution(new_id: String): String {
 
     //6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
     //만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-    if(answer.length > 16) answer = answer.substring(0,15)
-    if(answer[answer.length-1] == '.') answer = answer.substring(0,answer.length-2)
+    if(answer.length > 15) answer = answer.substring(0,15)
+    if(answer[answer.length-1] == '.') answer = answer.substring(0,answer.length-1)
 
     //7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
     if(answer.length <= 2) {

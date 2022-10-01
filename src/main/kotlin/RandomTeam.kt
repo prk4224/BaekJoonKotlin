8130eb9ab8518a09b6f4a6b00dbfd83b9d6873f9
박재홍 <prk0487@gmail.com>
2022-10-01 14:00:02 +0900

    RandomTeam

---
+++ src/main/kotlin/RandomTeam.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+
+// 팀원 수, 그룹 수, 멤버 수, 기간 ?
+fun main(){
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val st = StringTokenizer(br.readLine())
+
+    val teamMemeber = st.nextToken()
+    val groupCount = st.nextToken()
+    val totalMember = st.nextToken()
+    val totalWeek = st.nextToken()
+
+}

13e2b6c479a7717e8f57602cd4cb03d8dac9a136
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-22 15:57:20 +0900

    Coding Test

---
+++ src/main/kotlin/Test3.kt
@@ -0,0 +1,46 @@
+import kotlin.math.max
+fun solution(s: String): Int {
+    var answer = 1
+
+    for (i in 1 until s.length - 1) {
+        var cnt = 1
+        var leftIdx = i - 1
+        var rightIdx = i + 1
+        while (leftIdx >= 0 && rightIdx <= s.lastIndex) {
+            if (s[leftIdx] == s[rightIdx]) {
+                cnt += 2
+            } else {
+                break
+            }
+            leftIdx--
+            rightIdx++
+        }
+        answer = max(answer, cnt)
+    }
+
+    for (i in 0 until s.length - 1) {
+        if (s[i] != s[i + 1]) continue
+
+        val left = i
+        val right = i + 1
+
+        var cnt = 2
+
+        var leftIdx = left - 1
+        var rightIdx = right + 1
+
+        while (leftIdx >= 0 && rightIdx <= s.lastIndex) {
+            if (s[leftIdx] == s[rightIdx]) {
+                cnt += 2
+            } else {
+                break
+            }
+            leftIdx--
+            rightIdx++
+        }
+
+        answer = max(answer, cnt)
+    }
+
+    return answer
+}

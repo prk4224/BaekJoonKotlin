0dffdbc5afa7c5e23f2e939946f77476eaa2caf3
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-04-10 18:38:48 +0900

    프로그래머스 등산코드

--- src/main/kotlin/MountaineeringCourse.kt
+++ src/main/kotlin/MountaineeringCourse.kt
@@ -1,87 +1,94 @@
-var intensity = Int.MAX_VALUE
-var targetSummits = 0
-lateinit var checkedGrape: Array<Array<Boolean>>
-lateinit var courseGrape: Array<Array<Int>>
+import java.util.*
+import kotlin.collections.ArrayList
 
-fun main() {
-    val n = 6
+// https://tech.kakao.com/2022/07/13/2022-coding-test-summer-internship/
 
-    val paths = arrayOf(
-        intArrayOf(1, 2, 3),
-        intArrayOf(2, 3, 5),
-        intArrayOf(2, 4, 2),
-        intArrayOf(2, 5, 4),
-        intArrayOf(3, 4, 4),
-        intArrayOf(4, 5, 3),
-        intArrayOf(4, 6, 1),
-        intArrayOf(5, 6, 1),
-    )
-
-    val gates = intArrayOf(1, 3)
-    val summits = intArrayOf(5)
-    solution(n, paths, gates, summits)
-}
-
+private lateinit var graph: ArrayList<ArrayList<Node>>
 fun solution(n: Int, paths: Array<IntArray>, gates: IntArray, summits: IntArray): IntArray {
+    graph = ArrayList()
+    for (i in 0 until n + 1) {
+        graph.add(ArrayList())
+    }
+    for (path in paths) {
+        val s = path[0]
+        val e = path[1]
+        val w = path[2]
 
-    courseGrape = Array(n + 1) {
-        Array(n + 1) { 0 }
+        // 출입구일 경우 다른 곳으로만 갈 수 있는 단방향
+        // 산봉우리일 경우 특정 한 곳에서 산봉우리로 가는 단방향
+        if (isGate(s, gates) || isSummit(e, summits)) {
+            graph[s].add(Node(e, w))
+        } else if (isGate(e, gates) || isSummit(s, summits)) {
+            graph[e].add(Node(s, w))
+        } else {
+            // 일반 등산로일 경우 양방향
+            graph[s].add(Node(e, w))
+            graph[e].add(Node(s, w))
+        }
     }
 
-    for (i in paths.indices) {
-        courseGrape[paths[i][0]][paths[i][1]] = paths[i][2]
-        courseGrape[paths[i][1]][paths[i][0]] = paths[i][2]
-    }
-    gates.forEach {
-        initCheckedGrape(n)
-        findIntensity(n, it, gates.toList(), false, it, 0, summits.toList(), 0)
-    }
-
-    return intArrayOf(targetSummits, intensity)
+    // 정상까지 갔을 때 최소이면 돌아올 때도 같은 경로를 선택하면 되므로
+    // 정상까지 가는 경우만 고려
+    return dijkstra(n, gates, summits)
 }
 
-fun findIntensity(
-    n: Int,
-    gate: Int,
-    gates: List<Int>,
-    checkedStart: Boolean,
-    location: Int,
-    currentIntensity: Int,
-    summits: List<Int>,
-    summit: Int
-) {
-    if (gate == location && checkedStart) {
-        if (currentIntensity < intensity) {
-            intensity = currentIntensity
-            targetSummits = summit
-        }
-        if (currentIntensity == intensity) {
-            targetSummits = targetSummits.coerceAtMost(summit)
-        }
-        initCheckedGrape(n)
-        return
-    } else {
-        courseGrape[location].forEachIndexed { idx, value ->
-            val temp = if (summits.contains(location)) {
-                initCheckedGrape(n)
-                location
-            } else if(summit != 0) {
-                summit
-            } else {
-              0
-            }
-            if (value != 0 && checkedGrape[location][idx].not() && gates.contains(idx).not()) {
-                checkedGrape[location][idx] = true
-                checkedGrape[idx][location] = true
-                findIntensity(n, gate, gates, true, idx, currentIntensity.coerceAtLeast(value), summits, temp)
-                return
+private fun dijkstra(n: Int, gates: IntArray, summits: IntArray): IntArray {
+    val qu: Queue<Node> = LinkedList()
+    val intensity = IntArray(n + 1)
+    Arrays.fill(intensity, Int.MAX_VALUE)
+
+    // 출입구 전부를 큐에 넣음
+    for (gate in gates) {
+        qu.add(Node(gate, 0))
+        intensity[gate] = 0 // 시작 지점은 0
+    }
+    while (!qu.isEmpty()) {
+        val cn = qu.poll()
+
+        // 현재의 가중치가 저장된 가중치보다 커서 최소 갱신이 되지 않을 때 스킵
+        if (cn.w > intensity[cn.e]) continue
+        for (i in graph[cn.e].indices) {
+            val nn = graph[cn.e][i]
+
+            // 최소 갱신
+            val dis = Math.max(intensity[cn.e], nn.w)
+            if (intensity[nn.e] > dis) {
+                intensity[nn.e] = dis
+                qu.add(Node(nn.e, dis))
             }
         }
     }
+    var mn = Int.MAX_VALUE // 산봉우리 번호
+    var mw = Int.MAX_VALUE // 최솟값
+
+    // 정렬하지 않으면 12, 14, 15, 16, 17, 25번 문제 실패
+    Arrays.sort(summits)
+    for (summit in summits) {
+        if (intensity[summit] < mw) {
+            mn = summit
+            mw = intensity[summit]
+        }
+    }
+    return intArrayOf(mn, mw)
 }
 
-fun initCheckedGrape(n: Int) {
-    checkedGrape = Array(n + 1) {
-        Array(n + 1) { false }
+// num이 입구인지 확인
+private fun isGate(num: Int, gates: IntArray): Boolean {
+    for (gate in gates) {
+        if (num == gate) return true
     }
+    return false
 }
+
+// num이 산봉우리인지 확인
+private fun isSummit(num: Int, submits: IntArray): Boolean {
+    for (submit in submits) {
+        if (num == submit) return true
+    }
+    return false
+}
+
+private class Node internal constructor(
+    var e: Int, // 노드, 가중치
+    var w: Int
+)

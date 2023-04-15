ee3d0a5da490b86a8b41b4d7dbd13af4679e03bd
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-04-15 18:20:37 +0900

    프로그래머스 두원 사이 좌표갯수

--- src/main/kotlin/InterceptSystem.kt
+++ src/main/kotlin/InterceptSystem.kt
@@ -1,3 +1,7 @@
+import java.util.LinkedList
+import java.util.Queue
+import kotlin.math.pow
+
 class InterceptSystem {
     fun solution(targets: Array<IntArray>): Int {
         var answer = 1
@@ -5,14 +9,21 @@ class InterceptSystem {
             -it[0]
         }
 
-        var started = sortTargets[0][0]
+        val list = Array(5 + 1) { 0L }
+        list[1] = 5L
+        list[2] = 13L
 
+
+        var started = sortTargets[0][0]
+        val ttt = LinkedList<Queue<Pair<Int,Int>>>()
         sortTargets.forEach {
             if(it[1] <= started) {
                 answer += 1
                 started = it[0]
             }
         }
+        val temp = 2.0
+        val tmpe1 = temp.pow(2.0)
 
         return answer
     }
---
+++ src/main/kotlin/TwoCircleCoordinate.kt
@@ -0,0 +1,20 @@
+class TwoCircleCoordinate {
+    fun solution(r1: Int, r2: Int): Long {
+        var answer: Long = 0
+        val big = getCoordinateCount(r2)
+        val small = getCoordinateCount(r1)
+
+        return big - small + 4
+    }
+
+    fun getCoordinateCount(radius: Int): Long {
+
+        if(radius == 1) return 5L
+        if(radius == 2) return 13L
+        //.  24.36
+        // 5 13 29 49
+        //  8 16 20
+
+        return 0L
+    }
+}
